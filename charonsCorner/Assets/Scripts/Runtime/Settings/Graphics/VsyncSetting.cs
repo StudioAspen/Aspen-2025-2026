604bcac8126de20a86bfe1d70920c34ddecb1dc4
Samson Wu <78169553+samsonahh@users.noreply.github.com>
2025-07-17 01:37:47 -0400

    VSync setting

---
+++ charonsCorner/Assets/Scripts/Runtime/Settings/Graphics/VsyncSetting.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CharonsCorner.Runtime
+{
+    public class VsyncSetting : Setting
+    {
+        private protected override string playerPrefsKey => "Settings_Vsync";
+        private static readonly int defaultValue = 0;
+        public static int CurrentValue { get; private set; }
+
+        [SerializeField] private Toggle vsyncToggle;
+
+        private void OnEnable()
+        {
+            vsyncToggle.isOn = CurrentValue == 1;
+        }
+
+        public override void Load()
+        {
+            CurrentValue = PlayerPrefs.GetInt(playerPrefsKey, defaultValue);
+            vsyncToggle.isOn = CurrentValue == 1;
+        }
+
+        public override void Apply()
+        {
+            int vsyncValue = vsyncToggle.isOn ? 1 : 0;
+            PlayerPrefs.SetFloat(playerPrefsKey, vsyncValue);
+            CurrentValue = vsyncValue;
+            QualitySettings.vSyncCount = vsyncValue;
+        }
+
+        public override void Discard()
+        {
+            vsyncToggle.isOn = CurrentValue == 1;
+        }
+
+        public override bool IsDirty() => vsyncToggle.isOn != (CurrentValue == 1);
+    }
+}
