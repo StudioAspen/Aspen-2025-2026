5fa34fff0f7f3b128e2c7017fee7f67f4426867d
Samson Wu <78169553+samsonahh@users.noreply.github.com>
2025-09-22 17:14:38 -0400

    Replaced state player controller with procedural

--- charonsCorner/Assets/Scripts/Runtime/Player/PlayerController.cs
+++ charonsCorner/Assets/Scripts/Runtime/Player/PlayerController.cs
@@ -1,84 +1,117 @@
+using System;
 using NaughtyAttributes;
 using UnityEngine;
 
 namespace CharonsCorner.Runtime
 {
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(SphereCollider))]
     public class PlayerController : MonoBehaviour
     {
-        public InputManager Input { get; private set; } // For quick access
-        [field: SerializeField] public CharacterController Controller { get; private set; }
-        [field: SerializeField] public GameObject VisualObject { get; private set; }
-
-        public StateMachine<PlayerController> StateMachine { get; private set; }
-        [field: SerializeField] public GroundedSuperState GroundedSuperState { get; private set; } = new();
-        [field: SerializeField] public AirborneSuperState AirborneSuperState { get; private set; } = new();
-
-        [field: Header("Config")]
-        [field: SerializeField] public float StrictSpeedCap { get; private set; } = 100f;
+        private InputManager _input; // Easy access to InputManager.Instance
         
-        private Vector3 _velocity;
-        [ShowNativeProperty] public Vector3 Velocity => _velocity;
-        public Vector3 PlanarVelocity => _velocity.WithY(0f);
-        public float CurrentSpeed => _velocity.magnitude;
-        /// <summary>
-        /// Easy access to whether the player is grounded or not from the context. GroundedSuperState handles the actual checking.
-        /// </summary>
-        [ShowNativeProperty] public bool IsGrounded => GroundedSuperState.IsGrounded;
+        [Header("References")]
+        [SerializeField] private Rigidbody _rigidBody;
+        [SerializeField] private SphereCollider _sphereCollider;
+
+        [Header("Ground Detection")] 
+        [SerializeField] private LayerMask _groundLayer;
+        [SerializeField] private float _groundCheckYOffset = 0.25f;
+        [SerializeField] private float _groundCheckDistance = 0.5f;
+        [SerializeField] private float _regroundDelay = 0.05f;
+        [SerializeField, ReadOnly] private bool _isGrounded;
+        
+        [Header("Jump")]
+        [SerializeField] private float _jumpHeight = 2f;
+        [SerializeField] private float _coyoteTime = 0.4f;
+        private bool _hasJumped;
+        [SerializeField, ReadOnly] private float _inAirTime;
+        
+        [Header("Speed")]
+        [SerializeField] private float _speed = 5f;
 
         private void Awake()
         {
-            Input = InputManager.Instance;
+            _input = InputManager.Instance;
+        }
 
-            StateMachine = new StateMachine<PlayerController>(this);
-            InitializeStates(StateMachine, this);
+        private void OnEnable()
+        {
+            _input.Jump += HandleJumpInput;
+        }
+
+        private void OnDisable()
+        {
+            if(_input != null)
+            {
+                _input.Jump -= HandleJumpInput;
+            }
         }
 
         private void OnDestroy()
         {
-            StateMachine.Destroy();
-        }
-
-        private void OnDrawGizmos()
-        {
-            // Draws the ground check sphere in the editor for visualization
-            Gizmos.color = Color.red;
-            // Gizmos.DrawWireSphere(transform.position + GroundedSuperState. * Vector3.down, GroundedSuperState.GroundCheckRadius);
+            
         }
 
         private void Update()
         {
-            StateMachine.Update();
+            CheckGrounded();
+            UpdateInAirTime();
         }
 
         private void FixedUpdate()
         {
-            GroundedSuperState.CheckGrounded();
-
-            StateMachine.FixedUpdate();
+            Vector3 moveDirection = Utilities.GetCameraBasedMoveInput(CameraManager.Instance.CurrentCamera.transform,
+                _input.MoveDirection);
+            _rigidBody.AddForce(_speed * moveDirection, ForceMode.Acceleration);
         }
 
-        /// <summary>
-        /// Runs in awake. Sets up the state machine and all the states for the player controller.
-        /// </summary>
-        private void InitializeStates(StateMachine<PlayerController> machine, PlayerController context)
+        private void OnDrawGizmos()
         {
-            GroundedSuperState.Init(machine, context);
-            AirborneSuperState.Init(machine, context);
-
-            machine.ChangeState(GroundedSuperState, context);
+            Gizmos.color = Color.red;
+            Vector3 groundCheckStartPosition = transform.position + (_groundCheckYOffset - _sphereCollider.radius) * Vector3.up;
+            Gizmos.DrawLine(groundCheckStartPosition, groundCheckStartPosition + _groundCheckDistance * Vector3.down);
         }
 
-        public void SetVelocity(Vector3 newVelocity) => _velocity = newVelocity;
-        
-        public void SetPlanarVelocity(Vector3 newVelocity) => _velocity = newVelocity.WithY(_velocity.y);
-        
-        public void ApplyPlanarVelocity() => Controller.Move(Time.deltaTime * PlanarVelocity);
-
-        public void ApplyGravity()
+        private void CheckGrounded()
         {
-            if(!IsGrounded)
-                _velocity += Time.deltaTime * Physics.gravity;
-            Controller.Move(_velocity.y * Time.deltaTime * Vector3.up);
+            if (_inAirTime != 0f && _inAirTime < _regroundDelay)
+            {
+                _isGrounded = false;
+                return;
+            }
+            
+            _isGrounded = Physics.Raycast(transform.position + (_groundCheckYOffset - _sphereCollider.radius) * Vector3.up,
+                    Vector3.down, out RaycastHit hit, _groundCheckDistance, _groundLayer);
+        }
+        
+        private void HandleJumpInput()
+        {
+            if (_hasJumped)
+                return;
+
+            // Coyote time expired
+            if (_inAirTime != 0f && _inAirTime > _coyoteTime)
+                return;
+            
+            float jumpForce = Utilities.GetJumpForce(_jumpHeight);
+            _rigidBody.AddForce((jumpForce - _rigidBody.linearVelocity.y) * Vector3.up, ForceMode.VelocityChange);
+
+            _hasJumped = true;
+            _isGrounded = false;
+            _inAirTime = Mathf.Epsilon; // To activate the reground check and make _inAirTime > 0
+        }
+
+        private void UpdateInAirTime()
+        {
+            if (_isGrounded)
+            {
+                _inAirTime = 0f;
+                _hasJumped = false;
+                return;
+            }
+            
+            _inAirTime += Time.deltaTime;
         }
     }
 }
--- charonsCorner/Assets/Scripts/Runtime/Player/PlayerDebugUI.cs
+++ charonsCorner/Assets/Scripts/Runtime/Player/PlayerDebugUI.cs
@@ -6,25 +6,5 @@ namespace CharonsCorner.Runtime
     public class PlayerDebugUI : MonoBehaviour
     {
         [SerializeField] private PlayerController _playerController;
-        [SerializeField] private TMP_Text _superStateText;
-        [SerializeField] private TMP_Text _subStateText;
-        [SerializeField] private TMP_Text _speedText;
-
-        private void LateUpdate()
-        {
-            ShowStates();
-            _speedText.text = $"Speed: {Utilities.FloatToString(_playerController.CurrentSpeed, 2)}";
-        }
-
-        private void ShowStates()
-        {
-            string superStateName = _playerController.StateMachine.CurrentState.GetType().Name;
-            string subStateName = "None";
-            if(_playerController.StateMachine.CurrentState is SuperState<PlayerController> hierarchicalState && hierarchicalState.SubStateMachine.CurrentState != null)
-                subStateName = hierarchicalState.SubStateMachine.CurrentState.GetType().Name;
-
-            _superStateText.text = $"Superstate: {superStateName}";
-            _subStateText.text = $"Substate: {subStateName}";
-        }
     }
 }
