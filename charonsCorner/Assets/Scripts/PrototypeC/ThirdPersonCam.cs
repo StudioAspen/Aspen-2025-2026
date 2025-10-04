89f1e8b188d459ff9418d44e7d2189f15a032bf2
Plutzz <benjiongco@gmail.com>
2025-10-03 17:55:19 -0700

    Create Camera Script

---
+++ charonsCorner/Assets/Scripts/PrototypeC/ThirdPersonCam.cs
@@ -0,0 +1,30 @@
+using System;
+using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace CharonsCorner.PrototypeC
+{
+    public class ThirdPersonCam : MonoBehaviour
+    {
+        [field:SerializeField] public Transform Orientation { get; private set; }
+    
+        private void Awake()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        private void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        public void Update()
+        {
+            // Rotate orientation towards camera
+            Vector3 viewDir = Orientation.position - new Vector3(transform.position.x, Orientation.position.y, transform.position.z); 
+            Orientation.forward = viewDir.normalized;
+        }
+    }
+
+}
