447bac9b7d13800329ba9702cc3020f356054b14
JustinY <89873541+jyip808@users.noreply.github.com>
2025-10-07 14:36:57 -1000

    added dash item

---
+++ charonsCorner/Assets/Scripts/Runtime/PrototypeC/DashPower.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using CharonsCorner.ItemPowers;
+using CharonsCorner.Runtime;
+
+namespace CharonsCorner.Runtime
+{
+    public class DashPower : ItemPower
+    {
+        public float dashForce;
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        public override void itemPower(Rigidbody Rb)
+        {
+            GameObject go = Rb.gameObject;
+            //Vector3 myUp = go.transform.TransformDirection(Vector3.up);
+            //Rb.AddForce(myUp * jumpForce, ForceMode.Impulse);
+            Rb.AddForce(go.GetComponent<PrototypePlayerController>().Orientation.forward * dashForce, ForceMode.Impulse);
+        }
+    }
+}
