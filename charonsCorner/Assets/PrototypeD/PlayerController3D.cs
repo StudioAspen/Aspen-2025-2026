09c27748a8781c2637d2330681e17cb5640c20a9
Mario Vaquero <mariovquero@gmail.com>
2025-10-08 22:48:03 -0400

    disabled slow time

--- charonsCorner/Assets/PrototypeD/PlayerController3D.cs
+++ charonsCorner/Assets/PrototypeD/PlayerController3D.cs
@@ -308,7 +308,7 @@ public class PlayerController3D : MonoBehaviour
                 StartBrake();
 
                 // mess with timescale
-                dmScript.slowTime();
+                // dmScript.slowTime();
             }
         }
 
