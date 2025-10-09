e1f12d1a7726cca2aabecad47bd27510d1a43b80
Mikyle <mikylemosquera@gmail.com>
2025-10-09 15:34:28 -0700

    Added telemetry support to Blender and Export Manager

--- aspenTools/Blender/4.4/scripts/startup/aspen_startup.py
+++ aspenTools/Blender/4.4/scripts/startup/aspen_startup.py
@@ -17,12 +17,28 @@ def register():
         sys.path.append(PYTHON_PATH)
         sys.path.append(VENV_PATH)
 
-    import bqt
-    bqt.register()
+    from aspen.core.telemetry import init as telemetry
+    from aspen.core.excepthook import blender_excepthook
 
-    from aspen import blender_autoload as autoload
-    autoload.init()
-    autoload.register()
+    telemetry.initialize('blender')
+    sys.excepthook = blender_excepthook
+
+    from aspen.core.telemetry.trace import get_blender_tracer
+    _tracer = get_blender_tracer()
+
+    # Trace Blender Init
+    with _tracer.start_as_current_span('blender-init'):
+        # Trace BQT init
+        with _tracer.start_as_current_span('bqt-init'):
+            import bqt
+            bqt.register()
+
+        # Trace blender auto load
+        with _tracer.start_as_current_span('blender-autoload-init'):
+            from aspen import blender_autoload as autoload
+            autoload.init()
+            autoload.register()
+
 
 def unregister():
     import bqt
