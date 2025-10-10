74cc8083ecb4ce9e700112a240a752b2881c240b
JustinY <89873541+jyip808@users.noreply.github.com>
2025-10-09 14:51:29 -1000

    Shoot Item

---
+++ charonsCorner/Assets/Scripts/Runtime/PrototypeC/Items/ShootPower.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace CharonsCorner.Runtime
+{
+    public class ShootPower : MonoBehaviour
+    {
+        
+        public GameObject player;
+        public Vector3 playerOffset;
+        public GameObject firePoint;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
