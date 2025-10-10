cc4c2f097459f4143eb072ba393314dd354af7c9
ChillBeanJosh <joshover23@gmail.com>
2025-10-10 16:39:12 -0400

    Parameter Tweaks

--- charonsCorner/Assets/Scripts/Player/CameraLean.cs
+++ charonsCorner/Assets/Scripts/Player/CameraLean.cs
@@ -25,6 +25,8 @@ public class CameraLean : MonoBehaviour
     public void UpdateLean(float deltaTime, bool sliding, Vector3 acceleration, Vector3 up)
     {
         var planarAcceleration = Vector3.ProjectOnPlane(acceleration, up);
+
+        //Adjusts Damping based off current acceleration:
         var damping = planarAcceleration.magnitude > _dampedAcceleration.magnitude ? attackDamping : decayDamping;
 
         _dampedAcceleration = Vector3.SmoothDamp
@@ -43,11 +45,11 @@ public class CameraLean : MonoBehaviour
         //Reset Rotation to Parent Object:
         transform.localRotation = Quaternion.identity;
 
-        //Apply Rotation Axis:
+        //Smoothing:
         var targetLeanStrength = sliding ? slideLeanStrength : walkLeanStrength;
-
         _smoothLeanStrength = Mathf.Lerp(_smoothLeanStrength, targetLeanStrength, 1f - Mathf.Exp(-leanStrengthResponse * deltaTime));
 
+        //Apply Rotation Axis:
         transform.rotation = Quaternion.AngleAxis(_dampedAcceleration.magnitude * _smoothLeanStrength, leanAxis) * transform.rotation;
     }
 }
