96bf1e6f7b66b612893fa8ccb49e4d4443e0fb8f
JustinY <jyip808@hawaii.edu>
2025-10-13 20:08:37 -1000

    Fixed errors

--- charonsCorner/Assets/Scripts/Runtime/PrototypeC/Items/ShootPower.cs
+++ charonsCorner/Assets/Scripts/Runtime/PrototypeC/Items/ShootPower.cs
@@ -28,26 +28,24 @@ namespace CharonsCorner.Runtime
         // Update is called once per frame
         void Update()
         {
-            transform.position = player.transform.position + playerOffset;
-            Vector3 worldPos = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
-            transform.LookAt(worldPos);
-
-            if (Input.GetKeyDown(KeyCode.Space) &&  playerAbility.currentUses > 0)
+            if (player != null) 
             {
-                GameObject newBullet = Instantiate(bullet, firePoint.transform.position, firePoint.transform.rotation);
-                newBullet.GetComponent<Rigidbody>().AddForce(-newBullet.transform.forward * bulletForce, ForceMode.Impulse);
-                //timer = shootCoolDown;
-                playerAbility.currentUses--;
-                if (playerAbility.currentUses == 0)
+                transform.position = player.transform.position + playerOffset;
+                Vector3 worldPos = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+                transform.LookAt(worldPos);
+
+                if (Input.GetKeyDown(KeyCode.Space) && playerAbility.currentUses > 0)
                 {
-                    gameObject.SetActive(false);
+                    GameObject newBullet = Instantiate(bullet, firePoint.transform.position, firePoint.transform.rotation);
+                    newBullet.GetComponent<Rigidbody>().AddForce(-newBullet.transform.forward * bulletForce, ForceMode.Impulse);
+                    //timer = shootCoolDown;
+                    playerAbility.currentUses--;
+                    if (playerAbility.currentUses == 0)
+                    {
+                        gameObject.SetActive(false);
+                    }
                 }
             }
-
-            /*if (timer > 0)
-            {
-                timer -= Time.deltaTime;
-            }*/
         }
 
 
