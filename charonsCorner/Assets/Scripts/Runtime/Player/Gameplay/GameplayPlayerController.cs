890acf70c9cbb8f4d7c186573f5048a8a76c3175
Samson Wu <78169553+samsonahh@users.noreply.github.com>
2025-10-24 18:56:45 -0400

    Merge base player controller from C without powerups (#10)
    * Moved scripts, prefabs, and materials from C
    
    * Removed non-player content from C
    
    * Cleaned player controller
    
    Co-authored-by: Benjamin J. <benjiongco@gmail.com>
    Co-authored-by: Justin Y. <89873541+jyip808@users.noreply.github.com>

---
+++ charonsCorner/Assets/Scripts/Runtime/Player/Gameplay/GameplayPlayerController.cs
@@ -0,0 +1,95 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace CharonsCorner.Runtime
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class GameplayPlayerController : MonoBehaviour
+    {
+        [SerializeField] private float _gravityAmount = 30f;
+        
+        [Header("Ground Check")]
+        [SerializeField] private float _groundCheckLength = 0.5f;
+
+        [SerializeField] private LayerMask _groundLayer;
+        
+        [field: SerializeField] public Transform Orientation { get; private set; }
+
+        public Rigidbody Rb { get; private set; }
+        public SphereCollider Collider { get; private set; }
+        public SlopeSensor SlopeSensor { get; private set; }
+        public bool IsGrounded { get; private set; }
+
+        #region StateMachine Vars
+        public StateMachine<GameplayPlayerController> StateMachine { get; private set; }
+        
+        [field: Header("State Machine")]
+        [field: SerializeField] public GroundSuperState GroundState { get; private set; } = new();
+        [field: SerializeField] public AirSuperState AirState { get; private set; } = new();
+        
+        [NonSerialized] public String CurrentSubState;
+        #endregion
+
+        private void Awake()
+        {
+            Rb = GetComponent<Rigidbody>();
+            Collider = GetComponent<SphereCollider>();
+            SlopeSensor = GetComponentInChildren<SlopeSensor>();
+            SetupStateMachine();
+        }
+
+        private void Update()
+        {
+            StateMachine.Update();
+        }
+        
+        private void FixedUpdate()
+        {
+            ApplyGravity();
+            CheckGrounded();
+            StateMachine.FixedUpdate();
+        }
+
+        private void ApplyGravity()
+        {
+            Rb.AddForce(Vector3.down * _gravityAmount, ForceMode.Acceleration);
+        }
+        
+        private void CheckGrounded()
+        {
+            IsGrounded = Physics.CheckSphere(transform.position + Vector3.down * _groundCheckLength, Collider.radius * 0.9f, _groundLayer);
+        }
+
+        /// <summary>
+        /// Runs in awake. Sets up the state machine and all the states for the player controller.
+        /// </summary>
+        private void SetupStateMachine()
+        {
+            StateMachine = new StateMachine<GameplayPlayerController>(this);
+
+            GroundState.Init(StateMachine, this);
+            AirState.Init(StateMachine, this);
+            
+            StateMachine.ChangeState(GroundState, true);
+        }
+
+        private void OnDrawGizmos()
+        {
+        #if UNITY_EDITOR
+            if (Application.isPlaying)
+            {
+                Gizmos.color = IsGrounded ? Color.green : Color.red;
+                Gizmos.DrawWireSphere(transform.position - Vector3.up * _groundCheckLength, Collider.radius * 0.9f);
+                
+                GUIStyle style = new GUIStyle();
+                style.alignment = TextAnchor.MiddleCenter;
+                style.normal.textColor = Color.red;
+                style.fontSize = 40;
+                Handles.Label(transform.position + Vector3.up, 
+                    StateMachine.CurrentState.GetType().Name + ">" + CurrentSubState, style);
+            }
+        #endif
+        }
+    }
+}
---
+++ charonsCorner/Assets/Scripts/Runtime/Player/Gameplay/States/GroundSuperState.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+namespace CharonsCorner.Runtime
+{
+    [System.Serializable]
+    public class GroundSuperState : SuperState<GameplayPlayerController>
+    {
+        public override State<GameplayPlayerController> InitialSubState => MoveState;
+        [field: SerializeField] public GroundMoveState MoveState { get; private set; } = new();
+
+        private protected override void InitializeSubStates()
+        {
+            MoveState.Init(SubStateMachine, _context);
+        }
+
+        private protected override void OnEnter()
+        {
+            
+        }
+
+        private protected override void OnExit()
+        {
+           
+        }
+
+        private protected override void OnUpdate()
+        {
+        }
+
+        private protected override void OnFixedUpdate()
+        {
+
+        }
+
+        private protected override State<GameplayPlayerController> GetTransition()
+        {
+            if (!_context.IsGrounded)
+            {
+                return _context.AirState;
+            }
+            
+            return null;
+        }
+    }
+}
