fd580b9a1c5a28cc0df0e204ded8834bde699088
earthbound0 <158371482+earthbound0@users.noreply.github.com>
2025-11-13 20:12:25 -0500

    SAVE

---
+++ aspenTools/python/source/aspen/blender/rigging/rig_publish/ui.py
@@ -0,0 +1,31 @@
+import os
+
+import bpy
+
+from aspen.core.qt.singleton_main_window import SingletonMainWindow
+from aspen.core.qt import ui_loader
+
+import aspen.sitecustomize as sitecustomize
+
+from aspen.blender.common.export_manager import api
+# from . import (ASSET_TYPE_ENUM_ITEMS, EXPORT_TYPE_ENUM_ITEMS,
+               # EXPORT_TYPE_ENUM_MODEL, EXPORT_TYPE_ENUM_RIG, EXPORT_TYPE_ENUM_ANIMATION)
+
+from aspen.core.telemetry.loggers import get_blender_logger
+from aspen.core.telemetry import trace as aspen_trace
+from opentelemetry import trace
+_logger = get_blender_logger()
+
+class ExportManagerMainWindow(SingletonMainWindow):
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+
+        #for loading UI
+        ui_loader.load_ui(
+            os.path.join(os.path.dirname(__file__), 'ui/rig_publish.ui'),
+            self
+        )
+
+
+# )
