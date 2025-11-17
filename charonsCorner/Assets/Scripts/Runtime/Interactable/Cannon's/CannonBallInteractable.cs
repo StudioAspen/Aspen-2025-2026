92a1cf5d579ee6d61252ecec6dcb9597814a8866
Joshua Nunez <joshover23@gmail.com>
2025-11-16 19:52:11 -0500

    3 State CannonBall Suggestions
    - Note: any suggested changes for CannonBall.cs is not needed as we are no longer using this script.

--- charonsCorner/Assets/Scripts/Runtime/Interactable/Cannon's/CannonBallInteractable.cs
+++ charonsCorner/Assets/Scripts/Runtime/Interactable/Cannon's/CannonBallInteractable.cs
@@ -18,7 +18,9 @@ namespace CharonsCorner.Runtime
         public void HandleInteract()
         {
             if (_player == null || _cannonBall == null) return;
-            if (_player.CannonBallSuperState.LaunchCompleted == false && (_player.CannonBallSuperState.PillarMoveState.IsInCannon || _player.CannonBallSuperState.FiredState.IsLaunching)) return;
+
+            var cannonSuper = _player.CannonBallSuperState;
+            if (!cannonSuper.LaunchCompleted && (cannonSuper.EntryState.IsInCannon || cannonSuper.PillarMoveState.IsInCannon || cannonSuper.FiredState.IsLaunching)) return;
 
             ActivateCannon();
         }
