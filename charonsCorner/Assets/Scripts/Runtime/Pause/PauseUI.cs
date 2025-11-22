073e39387400c6d5b09aca39ae7607afbef61db9
Gloryhunter305 <mikylejosh@gmail.com>
2025-11-21 21:02:27 -0500

    11/21/25 9:01 pm
    -Implemented pause menu prototype
    - Added animation

--- charonsCorner/Assets/Scripts/Runtime/Pause/PauseUI.cs
+++ charonsCorner/Assets/Scripts/Runtime/Pause/PauseUI.cs
@@ -1,10 +1,14 @@
 using System;
+using DG.Tweening;
 using UnityEngine;
 
 namespace CharonsCorner.Runtime
 {
     public class PauseUI : UIPanel
     {
+        [SerializeField] private float _animationDuration = 0.5f;
+        [SerializeField] private Ease _animationEaseType = Ease.Linear;
+
         private protected override void Initialize()
         {
 
@@ -13,6 +17,12 @@ namespace CharonsCorner.Runtime
         private void OnEnable()
         {
             InputManager.Instance.Unpause += InputManager_Unpause;
+
+            RectTransform rt = GetComponent<RectTransform>();
+            rt.position = rt.position.WithY(Screen.height);
+            //rt.DOMoveY(Screen.height/2, _animationDuration).SetUpdate(true).SetEase(_animationEaseType).OnComplete(CloseUI).SetId(1);
+
+            rt.DOMoveY(Screen.height / 2, _animationDuration).SetUpdate(true).SetEase(_animationEaseType);
         }
 
         private void OnDisable()
@@ -35,6 +45,7 @@ namespace CharonsCorner.Runtime
 
         public override void CloseUI()
         {
+            Debug.Log("Closing Pause UI");
             GameManager.Instance.ChangeGameState(GameState.Gameplay);
         }
 
