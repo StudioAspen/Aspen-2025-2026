4033f5e655a1696e3e7980698b5b480b08d92402
Benjamin Jiongco <126136378+Plutzz@users.noreply.github.com>
2025-12-05 14:44:08 -0800

    Team c/sprint3 (#42)
    * properly calculate player score
    
    * Building first part of section
    
    * create scene
    
    * basic level layout
    
    * finish knitting level blockout
    
    * Finish first iteration
    
    * Bedroom section update
    
    * level iterating
    
    - skinnier spline
    - slight curve inwards on sharp turns
    
    * Finish merging Level 2 scenes
    
    * PositionSwitcher
    
    * Fix code convention issues
    
    * Update references
    
    * Made SpawnPointManager non-singleton, unsubbed from Respawn event on player destroy
    
    ---------
    
    Co-authored-by: Jeremy Chan <jeremynchan@gmail.com>
    Co-authored-by: Ayanna <ayanna.vasquez@egdcollective.org>
    Co-authored-by: Samson Wu <78169553+samsonahh@users.noreply.github.com>

--- charonsCorner/Assets/Scripts/Runtime/ScoringSystem/ScoringSystem.cs
+++ charonsCorner/Assets/Scripts/Runtime/ScoringSystem/ScoringSystem.cs
@@ -12,16 +12,15 @@ namespace CharonsCorner.Runtime
 
         private float _levelTimer;
         private int _score;
+        private bool _bonusApplied = false;
 
         public float LevelTimeSeconds => _levelTimer;
 
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
         
         }
 
-        // Update is called once per frame
         void Update()
         {
             _levelTimer += Time.deltaTime; // will probably need to be adjusted in the future
@@ -43,11 +42,11 @@ namespace CharonsCorner.Runtime
             for (int i = 0; i < _rankData.Criteria.Count; i++)
             {
                 var criteria = _rankData.Criteria[i];
-                int playerScore = _score;
 
-                if (_levelTimer <= criteria.MaxTimeSeconds)
+                if (_levelTimer <= criteria.MaxTimeSeconds && !_bonusApplied)
                 {
-                    playerScore += criteria.TimeBonusPoints;
+                    _score += criteria.TimeBonusPoints;
+                    _bonusApplied = true;
                 }
 
                 if (_score >= criteria.RequiredPoints)
