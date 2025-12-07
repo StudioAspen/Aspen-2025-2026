72f4359a541c9c3b551f77224ef13d32bad851c6
earthbound0 <158371482+earthbound0@users.noreply.github.com>
2025-12-07 18:49:52 -0500

    Update api.py

--- aspenTools/python/source/aspen/blender/rigging/publish_rig/api.py
+++ aspenTools/python/source/aspen/blender/rigging/publish_rig/api.py
@@ -1,5 +1,20 @@
-import trace
+import trace #search up what trace does and describe here.
+import os
+import bpy
+
+from aspen.core.telemetry import trace
 
 
-# @trace.Trace_blender_function()
-# def publish_rig():
+
+
+@trace.trace_blender_function()
+def publish_rig(file_path: str):
+    """Publish rig as a .blend file to the specified file path."""
+
+    # save_textures() # will need.
+    bpy.ops.wm.save_as_mainfile(filepath=file_path) # saves file as a .blend file.
+        # use_custom_props=True, # Custom properties will be applied to the current save
+
+
+
+    )
