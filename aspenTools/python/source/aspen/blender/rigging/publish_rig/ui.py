e35a53dd1d7ad970a9cd8e3231ac93e50c8ee665
earthbound0 <158371482+earthbound0@users.noreply.github.com>
2026-01-05 21:34:43 -0500

    save

--- aspenTools/python/source/aspen/blender/rigging/publish_rig/ui.py
+++ aspenTools/python/source/aspen/blender/rigging/publish_rig/ui.py
@@ -58,17 +58,18 @@ class PublishRigMainWindow(SingletonMainWindow):
                     raise Exception('File must be saved in order to export a model.')
 
                 # Get export settings
-                asset_name = publish_rig.asset_name.strip
-                publish_type = publish_rig.export_type
+                asset_name = publish_rig.asset_name.strip # !! probably will be an issue soon. Update: yeah
+                # publish_type = publish_rig
+                # publish_type = publish_rig.export_type
                 asset_type = f'{publish_rig.asset_type.lower()}s'
 
                 # Set the export directory based on export and asset type
                 if publish_type == PUBLISH_TYPE_ENUM_CHARACTER:
-                    publish_dir = os.path.join(sitecustomize.BLENDER_PROJECT_ASSETS_DIR, 'Art', 'models', asset_type,
+                    publish_dir = os.path.join(sitecustomize.ART_ASSETS_DIR, 'Art', 'models', asset_type,
                                               asset_name) # "Export to a folder of the rig's name"
                 elif publish_type == PUBLISH_TYPE_ENUM_ACTOR:
                     blend_dir = os.path.basename(os.path.dirname(bpy.data.filepath)) # gets the .blend file's current file path, apparently.
-                    publish_dir = os.path.join(sitecustomize.BLENDER_PROJECT_ASSETS_DIR, 'Art', 'animations',
+                    publish_dir = os.path.join(sitecustomize.ART_ASSETS_DIR, 'Art', 'animations',
                                               asset_type, blend_dir)
                 else:
                     # Cancel if unknown export type
@@ -90,4 +91,4 @@ class PublishRigMainWindow(SingletonMainWindow):
 
 
     def _asset_type_combo_box_changed(self, i: int):
-        print(ASSET_TYPE[i])
+        print(ASSET_TYPE[i]) # for testing
--- aspenTools/python/source/aspen/sitecustomize.py
+++ aspenTools/python/source/aspen/sitecustomize.py
@@ -7,9 +7,16 @@ PYTHON_DIR = os.path.join(REPO_DIR, 'aspenTools', 'python')
 PYTHON_IMAGES_DIR = os.path.join(PYTHON_DIR, 'images')
 UNITY_PROJECT_ASSETS_DIR = os.path.join(REPO_DIR, 'charonsCorner', 'Assets')
 
-BLENDER_PROJECT_ASSETS_DIR = os.path.join(REPO_DIR, 'art')
+ART_ASSETS_DIR = os.path.join(REPO_DIR, 'art')
 """Refers to the Artists' main directory (Aspen-2025-2026 > charonsCorner > art).
 To refer to subfolders specifically, write the following: 
 
-BLENDER_PROJECT_ASSETS_DIR, '[insert subfolder1's name here]', '[insert subfolder1's name here]', ...'
+os.path.join(sitecustomize.ART_ASSETS_DIR, '[insert subfolder1's name here]', '[insert subfolder1's name here]', ...')
+You can also substitute subfolder names for variables you created."""
+
+TECH_ART_BLENDER_ASSETS_DIR = os.path.join(REPO_DIR, 'techart')
+"""Refers to the Tech Artists' main directory (Aspen-2025-2026 > charonsCorner > techart).
+To refer to subfolders specifically, write the following: 
+
+os.path.join(sitecustomize.TECH_ART_BLENDER_ASSETS_DIR, '[insert subfolder1's name here]', '[insert subfolder1's name here]', ...')
 You can also substitute subfolder names for variables you created."""
