c3b2bc779a02c79b2c5cdfc1f7dc38e8368006d9
VictorWithaW <123853541+VictorWithaW@users.noreply.github.com>
2026-01-14 00:38:32 -0500

    Removing and Changing Unused Stuff

--- charonsCorner/Assets/Scripts/Runtime/Hub/HubEventList.cs
+++ charonsCorner/Assets/Scripts/Runtime/Hub/HubEventList.cs
@@ -2,11 +2,6 @@ namespace CharonsCorner.Runtime
 {
     public static class HubEventList
     {
-        public const string SceneStartToTitle = "Hub.SceneStart.To.Title";
-        public const string TitleToGameplay = "Hub.Title.To.Gameplay";
-        public const string ToCharon = "Hub.To.Charon";
-        public const string ToLanes = "Hub.To.Lanes";
-        
         public const string ShowTitleUI = "Hub.Show.TitleUI";
         public const string HideTitleUI = "Hub.Exit.TitleUI";
         public const string CameraToTitle = "Hub.Camera.MoveTo.Title";
