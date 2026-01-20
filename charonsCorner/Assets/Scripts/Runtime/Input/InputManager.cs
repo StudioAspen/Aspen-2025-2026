ce1eefcc201ffec1d7d06d701c80326574160465
Samson Wu <78169553+samsonahh@users.noreply.github.com>
2026-01-19 19:56:06 -0500

    UIManager overhaul with preparations for new Hub Level Select (#70)
    * Add level select ui script and organize hub scripts
    
    * UIManager -> UIPanel overhaul
    
    * Quit portal on left
    
    * Added a level select trigger in the hub

--- charonsCorner/Assets/Scripts/Runtime/Input/InputManager.cs
+++ charonsCorner/Assets/Scripts/Runtime/Input/InputManager.cs
@@ -8,7 +8,6 @@ using static CharonsCorner.Runtime.InputActions;
 
 namespace CharonsCorner.Runtime
 {
-
     public class InputManager : Singleton<InputManager>, IPlayerActions, IUIActions
     {
         [Header("References")]
@@ -197,7 +196,7 @@ namespace CharonsCorner.Runtime
             if (selectedObject == null)
                 return;
 
-            if (!UIManager.IsUIObjectInteractable(eventSystem, selectedObject))
+            if (!UIPanel.IsUIObjectInteractable(eventSystem, selectedObject))
                 return;
 
             ExecuteEvents.Execute(selectedObject, new BaseEventData(eventSystem), ExecuteEvents.submitHandler);
---
+++ charonsCorner/Assets/Scripts/Runtime/Input/UIInputBridge.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace CharonsCorner.Runtime
+{
+    [RequireComponent(typeof(InputManager))]
+    public class UIInputBridge : MonoBehaviour
+    {
+        private InputManager _inputManager;
+    
+        private void Start()
+        {
+            _inputManager = GetComponent<InputManager>();
+
+            _inputManager.OnControlSchemeChanged += InputManager_OnControlSchemeChanged;
+        
+            UIPanel.OnPanelChanged += UIPanel_OnPanelChanged;
+            UIPanel_OnPanelChanged(UIPanel.ActivePanel);
+        }
+
+        private void OnDestroy()
+        {
+            _inputManager.OnControlSchemeChanged -= InputManager_OnControlSchemeChanged;
+        
+            UIPanel.OnPanelChanged -= UIPanel_OnPanelChanged;
+        }
+
+        private void UIPanel_OnPanelChanged(UIPanel newPanel)
+        {
+            if (newPanel == null)
+            {
+                _inputManager.EnablePlayerActions();
+            }
+            else
+            {
+                _inputManager.EnableUIActions();
+            }
+        }
+    
+        private void InputManager_OnControlSchemeChanged(InputManager.ControlScheme newControlScheme)
+        {
+            UIPanel.SetCurrentSelectedObject();
+        }
+    }
+}
