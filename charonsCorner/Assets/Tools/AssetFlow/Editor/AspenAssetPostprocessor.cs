8ddc1980b5a876e3dffa2769c6098f74f57b29d5
Mikyle <mikylemosquera@gmail.com>
2026-02-03 19:59:23 -0800

    FIXED MATERIALS YAY

--- charonsCorner/Assets/Tools/AssetFlow/Editor/AspenAssetPostprocessor.cs
+++ charonsCorner/Assets/Tools/AssetFlow/Editor/AspenAssetPostprocessor.cs
@@ -112,6 +112,9 @@ namespace Aspen.Tools.Assets
 				return;
 			}
 
+			modelImporter.SearchAndRemapMaterials(ModelImporterMaterialName.BasedOnMaterialName,
+				ModelImporterMaterialSearch.Local);
+
 			// Extract textures from model
 			modelImporter.ExtractTextures(extractPath);
 			
@@ -178,7 +181,7 @@ namespace Aspen.Tools.Assets
 			if (description.TryGetProperty("DiffuseColor", out textureProperty))
 			{
 				// Assign the texture to the material.
-				material.SetTexture("_ColorMap", textureProperty.texture);
+				material.SetTexture("_BaseTexture", textureProperty.texture);
 			}
 		} 
 	}
