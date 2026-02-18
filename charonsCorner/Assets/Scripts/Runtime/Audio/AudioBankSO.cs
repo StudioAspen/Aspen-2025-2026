9ae1c5c8ad5d93ea9969734ee75e9ac495f3082d
Samson Wu <78169553+samsonahh@users.noreply.github.com>
2026-02-18 16:26:47 -0500

    One shot audio player and audiomanager improvements

---
+++ charonsCorner/Assets/Scripts/Runtime/Audio/AudioBankSO.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Animancer;
+using AYellowpaper.SerializedCollections;
+using UnityEngine;
+
+namespace CharonsCorner.Runtime
+{
+    [CreateAssetMenu(fileName = "AudioBank", menuName = "CharonsCorner/AudioBank")]
+    public class AudioBankSO : ScriptableObject
+    {
+        [SerializeField, SerializedDictionary("Audio ID", "Audio Clip")]
+        private SerializedDictionary<StringAsset, AudioClip> _bank;
+
+        public Dictionary<StringAsset, AudioClip> Bank => _bank;
+    }
+}
