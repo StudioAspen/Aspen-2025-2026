6bc4546885403da61e33e31d17d0fbd2482865d1
WiktorK01 <145882626+WiktorK01@users.noreply.github.com>
2026-03-04 15:07:45 -0500

    Level Progression Works

---
+++ charonsCorner/Assets/Scripts/Runtime/Hub/ProgressionModifier.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace CharonsCorner.Runtime
+{
+    /// <summary>
+    /// A simple script to modify the player's chapter progression via a public function.
+    /// Can be called from Unity Events (e.g., UI Buttons or Triggers).
+    /// </summary>
+    public class ProgressionModifier : MonoBehaviour
+    {
+        public void SetChapterIndex(int _targetChapterIndex)
+        {
+            SetChapterIndex(_targetChapterIndex);
+        }
+    }
+}
