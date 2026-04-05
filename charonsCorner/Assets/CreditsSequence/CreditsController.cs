37606e6c96894d56907ab6f2a259ddb70db4d7bf
Jason Padilla <84742412+Jayleaf1125@users.noreply.github.com>
2026-04-04 23:09:07 -0400

    create: Credits Sequence

---
+++ charonsCorner/Assets/CreditsSequence/CreditsController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CreditsController : MonoBehaviour
+{    
+    [Header("Settings"), Range(10f, 100f)]
+    [SerializeField] float _scrollSpeed;
+
+    RectTransform _rectTransform;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _rectTransform.anchoredPosition += new Vector2(0f, _scrollSpeed * Time.deltaTime);
+    }
+}
