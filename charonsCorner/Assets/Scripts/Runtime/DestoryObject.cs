4884d29705b9b5a138716d0d5d2017ea7bc3b217
Chanse21 <47192175+Chanse21@users.noreply.github.com>
2026-04-12 21:34:42 -0400

    Chanse Chpt2 Level2 Update Excube/Collider prefab, Destroy Object script

--- charonsCorner/Assets/Scripts/Runtime/DestoryObject.cs
+++ charonsCorner/Assets/Scripts/Runtime/DestoryObject.cs
@@ -6,21 +6,28 @@ namespace CharonsCorner.Runtime
     public class DestoryObject : MonoBehaviour
     {
         public GameObject targetObject;
-        public float hideTime = 2f; // seconds
+        //public float hideTime = 2f; // seconds
 
-        void OnTriggerEnter(Collider other)
-        {
-            if (other.CompareTag("Player"))
-            {
-                StartCoroutine(HideAndShow());
-            }
-        }
+        //void OnTriggerEnter(Collider other)
+        //{
+          //  if (other.CompareTag("Player"))
+            //{
+              //  StartCoroutine(HideAndShow());
+            //}
+        //}
 
-        IEnumerator HideAndShow()
+        //IEnumerator HideAndShow()
+        //{
+          //  targetObject.SetActive(false);
+            //yield return new WaitForSeconds(hideTime);
+            //targetObject.SetActive(true);
+        //}
+        void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("StairCollider"))
         {
-            targetObject.SetActive(false);
-            yield return new WaitForSeconds(hideTime);
-            targetObject.SetActive(true);
+            Destroy(gameObject);
         }
     }
+  }
 }
