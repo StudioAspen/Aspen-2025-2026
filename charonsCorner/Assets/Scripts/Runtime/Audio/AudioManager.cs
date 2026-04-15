6801764b202471eff275dc8c6bfe95b5157df92b
Samson Wu <78169553+samsonahh@users.noreply.github.com>
2026-04-15 15:01:31 -0400

    Added custom audio source

--- charonsCorner/Assets/Scripts/Runtime/Audio/AudioManager.cs
+++ charonsCorner/Assets/Scripts/Runtime/Audio/AudioManager.cs
@@ -132,7 +132,7 @@ namespace CharonsCorner.Runtime
             _musicSource.clip = null;
         }
         
-        private AudioMixerGroup GetMixerGroup(MixerTarget target)
+        public AudioMixerGroup GetMixerGroup(MixerTarget target)
         {
             if (target == MixerTarget.None) return null;
             if (target == MixerTarget.Default) return GetMixerGroup((MixerTarget)_defaultMixer);
@@ -163,5 +163,8 @@ namespace CharonsCorner.Runtime
         {
             Instance.MasterMixer.SetFloat(mixerParamName, ConvertFloatToDecibels(volumeTarget));
         }
+        
+        public bool TryGetClip(StringAsset id, out AudioClip clip)
+            => _soundBank.Bank.TryGetValue(id, out clip);
     }
 }
