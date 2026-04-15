c6f7bdd2a069b01152b2d527e0410f6c20250971
WiktorK01 <145882626+WiktorK01@users.noreply.github.com>
2026-04-15 03:05:23 -0400

    Hub Set-Dressing

--- charonsCorner/Assets/Scripts/Runtime/Hub/Components/LaneProgressDisplayHandler.cs
+++ charonsCorner/Assets/Scripts/Runtime/Hub/Components/LaneProgressDisplayHandler.cs
@@ -6,6 +6,7 @@ namespace CharonsCorner.Runtime
     {
         [SerializeField] private Light _spotlight;
         [SerializeField] private Canvas _inputDisplayCanvas;
+        [SerializeField] private Canvas _lockedDisplayCanvas;
 
         [SerializeField] private Color _lockedColor = Color.red;
         [SerializeField] private Color _unlockedColor = Color.white;
@@ -14,12 +15,14 @@ namespace CharonsCorner.Runtime
         {
             _spotlight.color = _unlockedColor;
             _inputDisplayCanvas.gameObject.SetActive(true);
+            _lockedDisplayCanvas.gameObject.SetActive(false);
         }
         
         public void ShowLocked()
         {
             _spotlight.color = _lockedColor;
             _inputDisplayCanvas.gameObject.SetActive(false);
+            _lockedDisplayCanvas.gameObject.SetActive(true);
         }
     }
 }
