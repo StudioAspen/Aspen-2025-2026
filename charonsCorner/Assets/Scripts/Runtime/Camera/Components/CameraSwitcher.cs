f55f209861db0fc1c58a8eedea40e822822b27d6
WiktorK01 <145882626+WiktorK01@users.noreply.github.com>
2026-05-13 00:58:19 -0400

    Tower

--- charonsCorner/Assets/Scripts/Runtime/Camera/Components/CameraSwitcher.cs
+++ charonsCorner/Assets/Scripts/Runtime/Camera/Components/CameraSwitcher.cs
@@ -9,17 +9,10 @@ namespace CharonsCorner.Runtime
         [SerializeField] private CinemachineBlendDefinition.Styles _blendType = CinemachineBlendDefinition.Styles.EaseOut;
         [SerializeField] private float _blendDuration = 0.5f;
 
-        private CinemachineCamera _defaultCamera;
-
         public float BlendDuration
         {
             get => _blendDuration;
             set => _blendDuration = value;
-        }
-
-        private void Start()
-        {
-            _defaultCamera = CameraManager.Instance.CurrentCamera;
         }
         
         [Button("Switch", ButtonSizes.Large)]
@@ -31,15 +24,7 @@ namespace CharonsCorner.Runtime
         [Button("Return To Default Camera", ButtonSizes.Large)]
         public void ReturnToDefaultCamera()
         {
-            if (_defaultCamera != null)
-            {
-                CameraManager.Instance.ChangeActiveCamera(_defaultCamera, _blendType, _blendDuration);
-            }
-            else
-            {
-                Debug.LogWarning("CameraSwitcher: No default camera tracked. Returning to SceneDefaultCamera instead.", this);
-                CameraManager.Instance.ResetActiveCamera();
-            }
+            CameraManager.Instance.ResetActiveCamera();
         }
     }
 }
