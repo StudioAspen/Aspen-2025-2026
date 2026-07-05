0420e9cba04fc0780b8df8ce99cdd779e9880c8e
WiktorK01 <145882626+WiktorK01@users.noreply.github.com>
2026-07-05 19:28:11 -0400

    Level 2 books fall

--- charonsCorner/Assets/Feel/MMFeedbacks/MMFeedbacks/Feedbacks/MMF_DestinationTransform.cs
+++ charonsCorner/Assets/Feel/MMFeedbacks/MMFeedbacks/Feedbacks/MMF_DestinationTransform.cs
@@ -23,9 +23,36 @@ namespace MoreMountains.Feedbacks
 		/// sets the inspector color for this feedback
 		#if UNITY_EDITOR
 		public override Color FeedbackColor { get { return MMFeedbacksInspectorColors.TransformColor; } }
-		public override bool EvaluateRequiresSetup() { return (TargetTransform == null) || (Destination == null); }
-		public override string RequiredTargetText { get { return TargetTransform != null ? TargetTransform.name : "";  } }
-		public override string RequiresSetupText { get { return "This feedback requires that a TargetTransform and a Destination be set to be able to work properly. You can set one below."; } }
+		public override bool EvaluateRequiresSetup() 
+		{
+			if (UseMultiTargets)
+			{
+				return (MultiTargetTransforms == null) || (MultiTargetTransforms.Count == 0) || (MultiDestinations == null) || (MultiDestinations.Count == 0);
+			}
+			return (TargetTransform == null) || (Destination == null); 
+		}
+		public override string RequiredTargetText 
+		{ 
+			get 
+			{
+				if (UseMultiTargets)
+				{
+					return MultiTargetTransforms != null ? MultiTargetTransforms.Count + " targets" : "0 targets";
+				}
+				return TargetTransform != null ? TargetTransform.name : "";  
+			} 
+		}
+		public override string RequiresSetupText 
+		{ 
+			get 
+			{
+				if (UseMultiTargets)
+				{
+					return "This feedback requires that MultiTargetTransforms and MultiDestinations be set to be able to work properly. You can set them below.";
+				}
+				return "This feedback requires that a TargetTransform and a Destination be set to be able to work properly. You can set one below."; 
+			} 
+		}
 		public override bool HasCustomInspectors { get { return true; } }
 		#endif
 		public override bool HasAutomatedTargetAcquisition => true;
@@ -34,7 +61,20 @@ namespace MoreMountains.Feedbacks
 		[MMFInspectorGroup("Target to animate", true, 61, true)]
 		/// the target transform we want to animate properties on
 		[Tooltip("the target transform we want to animate properties on")]
+		[MMFCondition("UseMultiTargets", false)]
 		public Transform TargetTransform;
+
+		/// whether or not to use the multi-target lists below
+		[Tooltip("whether or not to use the multi-target lists below")]
+		public bool UseMultiTargets = false;
+		/// the list of transforms to animate
+		[Tooltip("the list of transforms to animate")]
+		[MMFCondition("UseMultiTargets", true)]
+		public List<Transform> MultiTargetTransforms;
+		/// the list of destination transforms whose properties we want to match
+		[Tooltip("the list of destination transforms whose properties we want to match")]
+		[MMFCondition("UseMultiTargets", true)]
+		public List<Transform> MultiDestinations;
         
 		/// whether or not we want to force an origin transform. If not, the current position of the target transform will be used as origin instead
 		[Tooltip("whether or not we want to force an origin transform. If not, the current position of the target transform will be used as origin instead")]
@@ -45,6 +85,7 @@ namespace MoreMountains.Feedbacks
 		public Transform Origin;
 		/// the destination transform whose properties we want to match 
 		[Tooltip("the destination transform whose properties we want to match")]
+		[MMFCondition("UseMultiTargets", false)]
 		public Transform Destination;
         
 		[MMFInspectorGroup("Transition", true, 63)]
@@ -140,6 +181,16 @@ namespace MoreMountains.Feedbacks
 		protected Vector3 _initialPosition;
 		protected Vector3 _initialScale;
 		protected Quaternion _initialRotation;
+
+		protected List<Vector3> _initialPositions;
+		protected List<Vector3> _initialScales;
+		protected List<Quaternion> _initialRotations;
+		protected List<Vector3> _pointAPositions;
+		protected List<Vector3> _pointBPositions;
+		protected List<Quaternion> _pointARotations;
+		protected List<Quaternion> _pointBRotations;
+		protected List<Vector3> _pointAScales;
+		protected List<Vector3> _pointBScales;
         
 		/// <summary>
 		/// On Play we animate the pos/rotation/scale of the target transform towards its destination
@@ -148,10 +199,26 @@ namespace MoreMountains.Feedbacks
 		/// <param name="feedbacksIntensity"></param>
 		protected override void CustomPlayFeedback(Vector3 position, float feedbacksIntensity = 1.0f)
 		{
-			if (!Active || !FeedbackTypeAuthorized || (TargetTransform == null))
+			if (!Active || !FeedbackTypeAuthorized)
 			{
 				return;
 			}
+
+			if (UseMultiTargets)
+			{
+				if (MultiTargetTransforms == null || MultiTargetTransforms.Count == 0)
+				{
+					return;
+				}
+			}
+			else
+			{
+				if (TargetTransform == null)
+				{
+					return;
+				}
+			}
+			
 			if (_coroutine != null) { Owner.StopCoroutine(_coroutine); }
 			_coroutine = Owner.StartCoroutine(AnimateToDestination());
 		}
@@ -162,13 +229,20 @@ namespace MoreMountains.Feedbacks
 		/// <returns></returns>
 		protected virtual IEnumerator AnimateToDestination()
 		{
-			_initialPosition = TargetTransform.position;
-			_initialRotation = TargetTransform.rotation;
-			_initialScale = TargetTransform.localScale;
+			if (UseMultiTargets)
+			{
+				PrepareMultiTargets();
+			}
+			else
+			{
+				_initialPosition = TargetTransform.position;
+				_initialRotation = TargetTransform.rotation;
+				_initialScale = TargetTransform.localScale;
 
-			_pointAPosition = ForceOrigin ? Origin.transform.position : TargetTransform.position;
-			_pointARotation = ForceOrigin ? Origin.transform.rotation : TargetTransform.rotation;
-			_pointAScale = ForceOrigin ? Origin.transform.localScale : TargetTransform.localScale;
+				_pointAPosition = ForceOrigin ? Origin.transform.position : TargetTransform.position;
+				_pointARotation = ForceOrigin ? Origin.transform.rotation : TargetTransform.rotation;
+				_pointAScale = ForceOrigin ? Origin.transform.localScale : TargetTransform.localScale;
+			}
 			
 			CacheDestinationValues();
 
@@ -194,8 +268,53 @@ namespace MoreMountains.Feedbacks
 			yield break;
 		}
 
+		protected virtual void PrepareMultiTargets()
+		{
+			if (_initialPositions == null) { _initialPositions = new List<Vector3>(); }
+			if (_initialRotations == null) { _initialRotations = new List<Quaternion>(); }
+			if (_initialScales == null) { _initialScales = new List<Vector3>(); }
+			if (_pointAPositions == null) { _pointAPositions = new List<Vector3>(); }
+			if (_pointARotations == null) { _pointARotations = new List<Quaternion>(); }
+			if (_pointAScales == null) { _pointAScales = new List<Vector3>(); }
+			
+			_initialPositions.Clear();
+			_initialRotations.Clear();
+			_initialScales.Clear();
+			_pointAPositions.Clear();
+			_pointARotations.Clear();
+			_pointAScales.Clear();
+
+			for (int i = 0; i < MultiTargetTransforms.Count; i++)
+			{
+				if (MultiTargetTransforms[i] == null)
+				{
+					_initialPositions.Add(Vector3.zero);
+					_initialRotations.Add(Quaternion.identity);
+					_initialScales.Add(Vector3.one);
+					_pointAPositions.Add(Vector3.zero);
+					_pointARotations.Add(Quaternion.identity);
+					_pointAScales.Add(Vector3.one);
+					continue;
+				}
+
+				_initialPositions.Add(MultiTargetTransforms[i].position);
+				_initialRotations.Add(MultiTargetTransforms[i].rotation);
+				_initialScales.Add(MultiTargetTransforms[i].localScale);
+
+				_pointAPositions.Add(ForceOrigin ? Origin.transform.position : MultiTargetTransforms[i].position);
+				_pointARotations.Add(ForceOrigin ? Origin.transform.rotation : MultiTargetTransforms[i].rotation);
+				_pointAScales.Add(ForceOrigin ? Origin.transform.localScale : MultiTargetTransforms[i].localScale);
+			}
+		}
+
 		protected virtual void CacheDestinationValues()
 		{
+			if (UseMultiTargets)
+			{
+				CacheMultiDestinationValues();
+				return;
+			}
+
 			_pointBPosition = Destination.transform.position;
 
 			if (!AnimatePositionX) { _pointAPosition.x = TargetTransform.position.x; _pointBPosition.x = _pointAPosition.x; }
@@ -216,24 +335,116 @@ namespace MoreMountains.Feedbacks
 			if (!AnimateScaleZ) { _pointAScale.z = TargetTransform.localScale.z; _pointBScale.z = _pointAScale.z; }
 		}
 
+		protected virtual void CacheMultiDestinationValues()
+		{
+			if (_pointBPositions == null) { _pointBPositions = new List<Vector3>(); }
+			if (_pointBRotations == null) { _pointBRotations = new List<Quaternion>(); }
+			if (_pointBScales == null) { _pointBScales = new List<Vector3>(); }
+
+			_pointBPositions.Clear();
+			_pointBRotations.Clear();
+			_pointBScales.Clear();
+
+			for (int i = 0; i < MultiTargetTransforms.Count; i++)
+			{
+				if (MultiTargetTransforms[i] == null)
+				{
+					_pointBPositions.Add(Vector3.zero);
+					_pointBRotations.Add(Quaternion.identity);
+					_pointBScales.Add(Vector3.one);
+					continue;
+				}
+
+				Transform destination = GetDestination(i);
+				if (destination == null)
+				{
+					_pointBPositions.Add(MultiTargetTransforms[i].position);
+					_pointBRotations.Add(MultiTargetTransforms[i].rotation);
+					_pointBScales.Add(MultiTargetTransforms[i].localScale);
+					continue;
+				}
+
+				Vector3 pointBPosition = destination.position;
+				Vector3 pointAPosition = _pointAPositions[i];
+				if (!AnimatePositionX) { pointAPosition.x = MultiTargetTransforms[i].position.x; pointBPosition.x = pointAPosition.x; }
+				if (!AnimatePositionY) { pointAPosition.y = MultiTargetTransforms[i].position.y; pointBPosition.y = pointAPosition.y; }
+				if (!AnimatePositionZ) { pointAPosition.z = MultiTargetTransforms[i].position.z; pointBPosition.z = pointAPosition.z; }
+				_pointAPositions[i] = pointAPosition;
+				_pointBPositions.Add(pointBPosition);
+
+				Quaternion pointBRotation = destination.rotation;
+				Quaternion pointARotation = _pointARotations[i];
+				if (!AnimateRotationX) { pointARotation.x = MultiTargetTransforms[i].rotation.x; pointBRotation.x = pointARotation.x; }
+				if (!AnimateRotationY) { pointARotation.y = MultiTargetTransforms[i].rotation.y; pointBRotation.y = pointARotation.y; }
+				if (!AnimateRotationZ) { pointARotation.z = MultiTargetTransforms[i].rotation.z; pointBRotation.z = pointARotation.z; }
+				if (!AnimateRotationW) { pointARotation.w = MultiTargetTransforms[i].rotation.w; pointBRotation.w = pointARotation.w; }
+				_pointARotations[i] = pointARotation;
+				_pointBRotations.Add(pointBRotation);
+
+				Vector3 pointBScale = destination.localScale;
+				Vector3 pointAScale = _pointAScales[i];
+				if (!AnimateScaleX) { pointAScale.x = MultiTargetTransforms[i].localScale.x; pointBScale.x = pointAScale.x; }
+				if (!AnimateScaleY) { pointAScale.y = MultiTargetTransforms[i].localScale.y; pointBScale.y = pointAScale.y; }
+				if (!AnimateScaleZ) { pointAScale.z = MultiTargetTransforms[i].localScale.z; pointBScale.z = pointAScale.z; }
+				_pointAScales[i] = pointAScale;
+				_pointBScales.Add(pointBScale);
+			}
+		}
+
+		protected virtual Transform GetDestination(int index)
+		{
+			if (MultiDestinations == null || MultiDestinations.Count == 0)
+			{
+				return null;
+			}
+			if (index >= MultiDestinations.Count)
+			{
+				return MultiDestinations[MultiDestinations.Count - 1];
+			}
+			return MultiDestinations[index];
+		}
+
 		/// <summary>
 		/// Computes the new position, rotation and scale for our transform, and applies it to the transform
 		/// </summary>
 		/// <param name="percent"></param>
 		protected virtual void ChangeTransformValues(float percent)
 		{
-			_animationTweenType = SeparatePositionCurve ? AnimatePositionTween : GlobalAnimationTween;
-			_newPosition = Vector3.LerpUnclamped(_pointAPosition, _pointBPosition, _animationTweenType.Evaluate(percent));
+			if (UseMultiTargets)
+			{
+				for (int i = 0; i < MultiTargetTransforms.Count; i++)
+				{
+					if (MultiTargetTransforms[i] == null) { continue; }
+
+					_animationTweenType = SeparatePositionCurve ? AnimatePositionTween : GlobalAnimationTween;
+					_newPosition = Vector3.LerpUnclamped(_pointAPositions[i], _pointBPositions[i], _animationTweenType.Evaluate(percent));
                 
-			_animationTweenType = SeparateRotationCurve ? AnimateRotationTween : GlobalAnimationTween;
-			_newRotation = Quaternion.LerpUnclamped(_pointARotation, _pointBRotation, _animationTweenType.Evaluate(percent));
+					_animationTweenType = SeparateRotationCurve ? AnimateRotationTween : GlobalAnimationTween;
+					_newRotation = Quaternion.LerpUnclamped(_pointARotations[i], _pointBRotations[i], _animationTweenType.Evaluate(percent));
                 
-			_animationTweenType = SeparateScaleCurve ? AnimateScaleTween : GlobalAnimationTween;
-			_newScale = Vector3.LerpUnclamped(_pointAScale, _pointBScale, _animationTweenType.Evaluate(percent));
+					_animationTweenType = SeparateScaleCurve ? AnimateScaleTween : GlobalAnimationTween;
+					_newScale = Vector3.LerpUnclamped(_pointAScales[i], _pointBScales[i], _animationTweenType.Evaluate(percent));
 			
-			TargetTransform.position = _newPosition;
-			TargetTransform.rotation = _newRotation;
-			TargetTransform.localScale = _newScale;
+					MultiTargetTransforms[i].position = _newPosition;
+					MultiTargetTransforms[i].rotation = _newRotation;
+					MultiTargetTransforms[i].localScale = _newScale;
+				}
+			}
+			else
+			{
+				_animationTweenType = SeparatePositionCurve ? AnimatePositionTween : GlobalAnimationTween;
+				_newPosition = Vector3.LerpUnclamped(_pointAPosition, _pointBPosition, _animationTweenType.Evaluate(percent));
+                
+				_animationTweenType = SeparateRotationCurve ? AnimateRotationTween : GlobalAnimationTween;
+				_newRotation = Quaternion.LerpUnclamped(_pointARotation, _pointBRotation, _animationTweenType.Evaluate(percent));
+                
+				_animationTweenType = SeparateScaleCurve ? AnimateScaleTween : GlobalAnimationTween;
+				_newScale = Vector3.LerpUnclamped(_pointAScale, _pointBScale, _animationTweenType.Evaluate(percent));
+			
+				TargetTransform.position = _newPosition;
+				TargetTransform.rotation = _newRotation;
+				TargetTransform.localScale = _newScale;
+			}
 		}
 
 		/// <summary>
@@ -251,7 +462,7 @@ namespace MoreMountains.Feedbacks
 			base.CustomStopFeedback(position, feedbacksIntensity);
 			IsPlaying = false;
             
-			if ((TargetTransform != null) && (_coroutine != null))
+			if (_coroutine != null)
 			{
 				Owner.StopCoroutine(_coroutine);
 			}
@@ -266,9 +477,33 @@ namespace MoreMountains.Feedbacks
 			{
 				return;
 			}
-			TargetTransform.position = _initialPosition;
-			TargetTransform.rotation = _initialRotation;
-			TargetTransform.localScale = _initialScale;
+
+			if (UseMultiTargets)
+			{
+				if (MultiTargetTransforms == null || _initialPositions == null || MultiTargetTransforms.Count != _initialPositions.Count)
+				{
+					return;
+				}
+
+				for (int i = 0; i < MultiTargetTransforms.Count; i++)
+				{
+					if (MultiTargetTransforms[i] != null)
+					{
+						MultiTargetTransforms[i].position = _initialPositions[i];
+						MultiTargetTransforms[i].rotation = _initialRotations[i];
+						MultiTargetTransforms[i].localScale = _initialScales[i];
+					}
+				}
+			}
+			else
+			{
+				if (TargetTransform != null)
+				{
+					TargetTransform.position = _initialPosition;
+					TargetTransform.rotation = _initialRotation;
+					TargetTransform.localScale = _initialScale;
+				}
+			}
 		}
 		
 		/// <summary>
